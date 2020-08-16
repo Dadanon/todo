bb8c876c748c9e55d505633540e029c71b7235e3
Dadanon <mar4ello19922@gmail.com>
2020-08-16 22:35:43 +0300

    Initial commit

---
+++ todos/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title', )
+
+# Create your models here.
